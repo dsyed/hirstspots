a67e047cc9da0d11791e5862ff3553711656f9a1
Daanyaal Syed <daanyaalasyed@gmail.com>
2020-06-13 20:05:15 -0400

    Brighten default canvas background color

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as d3 from "d3";
 import { Grid } from "./models/grid";
 
-let grid = new Grid(4, 5, 20, 60, 45, "#F6F6F6");
+let grid = new Grid(4, 5, 20, 60, 45, "#fafafa");
 d3.select("#grid").append(() => grid.render());
