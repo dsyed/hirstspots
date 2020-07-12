2b998dc879170f16702c79b4117612e4dcdadab4
Daanyaal Syed <daanyaalasyed@gmail.com>
2020-07-11 22:21:59 -0400

    Add title, label, and drop shadows

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as d3 from "d3";
 import { Grid } from "./models/grid";
 
-let grid = new Grid(4, 5, 20, 60, 45, "#fafafa");
+let grid = new Grid(4, 5, 20, 60, 45, "#fbfbf9");
 d3.select("#grid").append(() => grid.render());
